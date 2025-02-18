49265f9a617b748ba782bb8a763aaf7c4f271013
Luiz Gustavo S. Real <lgsreal2@gmail.com>
2025-02-18 08:22:31 -0300

    aula 14-02-25

--- src/main/java/br/com/fiap/spring_mvc/repository/LivroRepository.java
+++ src/main/java/br/com/fiap/spring_mvc/repository/LivroRepository.java
@@ -1,5 +1,10 @@
 package br.com.fiap.spring_mvc.repository;
 
-public interface LivroRepository {
+import br.com.fiap.spring_mvc.model.Livro;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Livro, Long> {
 
 }
